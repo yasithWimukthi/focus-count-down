572df9408f6459a4d5c4cb89834348b80f6da7d1
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-22 17:02:10 +0530

    implement focus component

--- src/features/focus/focus.js
+++ src/features/focus/focus.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {View,Text,StyleSheet} from 'react-native';
+import {TextInput} from 'react-native-paper';
 
 export const Focus = () =>{
     return(
         <View style={styles.container}>
-            <Text>What would you like to focus on ?</Text>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>What would you like to focus on ?</Text>
+                <View style={styles.inputContainer}>
+                    <TextInput/>
+                </View>
+            </View>
         </View>
     )
 }
@@ -14,4 +20,17 @@ const styles = StyleSheet.create({
         flex: 1,
         padding:50
     },
+    titleContainer:{
+        flex:0.5,
+        padding:16,
+        justifyContent: 'center'
+    },
+    title:{
+        color:'white',
+        fontWeight:'bold',
+        fontSize:24
+    },
+    inputContainer:{
+        paddingTop:20
+    }
 });

36952e3a9e28bc6879659b80bd9f51f112846b23
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-24 19:56:24 +0530

    implement timer function

--- App.js
+++ App.js
@@ -2,21 +2,22 @@ import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Focus} from "./src/features/focus/focus";
 import {colors} from "./src/utils/colors";
+import {Timer} from "./src/features/timer/timer";
+import {spacing} from "./src/utils/sizes";
 
 export default function App() {
 
-  const [focusSubject,setFocusSubject] = useState(null);
+  const [focusSubject,setFocusSubject] = useState("null");
 
   return (
     <View style={styles.container}>
       {
         focusSubject ? (
-            <Text>build timer</Text>
+            <Timer focusSubject={focusSubject}/>
         ):(
             <Focus addSubject={setFocusSubject}/>
         )
       }
-      <Text>{focusSubject}</Text>
     </View>
   );
 }
@@ -24,6 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:colors.darkBlue
+    backgroundColor:colors.darkBlue,
+    paddingTop:spacing.md
   },
 });
